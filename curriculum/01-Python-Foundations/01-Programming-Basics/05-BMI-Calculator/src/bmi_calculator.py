1fac5aee1c143a9ba800f12c71e21a829ff939fb
gpelicant <gpelicant@tradelab.fr>
2019-04-02 12:22:12 +0200

    exo 05 from 01-programming-basics (bmi_calculator)

--- curriculum/01-Python-Foundations/01-Programming-Basics/05-BMI-Calculator/src/bmi_calculator.py
+++ curriculum/01-Python-Foundations/01-Programming-Basics/05-BMI-Calculator/src/bmi_calculator.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # TODO: Write a docstring
-
+"""
+A program which computes BMI
+"""
 
 def main():
     # TODO: input for name: name
-
+    name = input("What's your name? ").capitalize()
     # TODO: input for height in centimeters: height
-
+    height = int(input("How tall are you (in cm)? "))
     # TODO: input for weight: weight
-
+    weight = int(input("How much do you weight (in kg)? "))
     # TODO: compute the BMI: bmi
-
+    bmi = weight / (height / 100)**2
     # TODO: print a greeting to the user followed by it's BMI
     # Example: "Hello Georges, your BMI is 26.75!"
+    print('Hello {}, your BMI is {:.2f}!'.format(name, bmi))
 
 
 if __name__ == '__main__':
