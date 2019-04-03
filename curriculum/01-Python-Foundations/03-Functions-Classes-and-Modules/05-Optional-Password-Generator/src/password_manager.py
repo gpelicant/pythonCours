29550d1ae167b4808cac75369d66a39b6094e938
gpelicant <gpelicant@tradelab.fr>
2019-04-03 15:49:05 +0200

    exo 5 from part 3

--- curriculum/01-Python-Foundations/03-Functions-Classes-and-Modules/05-Optional-Password-Generator/src/password_manager.py
+++ curriculum/01-Python-Foundations/03-Functions-Classes-and-Modules/05-Optional-Password-Generator/src/password_manager.py
@@ -1,1 +1,33 @@
 # TODO: implement the exercices
+"""
+    my password generation
+"""
+import random
+import string
+
+def generate_password(n_characters= 20):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(n_characters))
+
+class PasswordRecord(object):
+    """
+        class for record password
+    """
+    def __init__(self, url, password=None):
+        self.url = url
+        self.password = password if password is not None else generate_password()
+
+class PasswordManager(object):
+    """
+        class for manage password
+    """
+    passwords = []
+
+    def __init__(self, passwords=[]):
+        pass
+
+    def add_password(self, value):
+        if isinstance(value, PasswordRecord):
+            self.passwords.append(value)
+        else:
+            self.passwords.append(PasswordRecord('url.com'))
