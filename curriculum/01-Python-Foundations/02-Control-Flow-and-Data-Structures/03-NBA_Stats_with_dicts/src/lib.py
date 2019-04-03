49a40635ea1f087288bd5c5fa45d9301332ff3fd
gpelicant <gpelicant@tradelab.fr>
2019-04-03 10:01:12 +0200

    exo 3 from part 2

--- curriculum/01-Python-Foundations/02-Control-Flow-and-Data-Structures/03-NBA_Stats_with_dicts/src/lib.py
+++ curriculum/01-Python-Foundations/02-Control-Flow-and-Data-Structures/03-NBA_Stats_with_dicts/src/lib.py
@@ -13,24 +13,39 @@ def main():
 
     # TODO: Create a dictionary using players and points where players are the
     # keys and values are the points
+    stats = dict(zip(players, points))
     # TODO: Call this dictionary `stats` then print it out
+    print(stats)
     # NOTE: [Advanced] Take a look at python's `zip` in the Python's doc,
     # that should allow you to
     # implement this in only one line
 
     # TODO: What was Stephen Curry average points?
-
+    print(stats['Stephen Curry'])
     # TODO: try to print Michael Jordan's average point
+    print(stats.get("Michael Jordan's"))
     # YOU SHOULD NOT RAISE AN ERROR!
 
     # TODO: Which player did score 24.9 ?
-
+    whichPlayer = ''
+    for player, point in stats.items():
+        if(point == 24.9):
+            whichPlayer = player
+            break
+    print(player)
     # A played is selected if it got a score above 26
     # TODO: Create a dict where keys are the names of the players and values
     #       are True if the player is selected and False otherwise
+    playerBoolean = [True if point > 26 else False for player, point in stats.items()]
+    stats2 = dict(zip(players, playerBoolean))
+    print(stats2)
 
     # TODO: Build a list of the names of the selected players: selection
-
+    playerSelected = []
+    for player, boolean in stats2.items():
+        if(boolean == True):
+            playerSelected.append(player)
+    print(playerSelected)
 
 if __name__ == '__main__':
     main()
