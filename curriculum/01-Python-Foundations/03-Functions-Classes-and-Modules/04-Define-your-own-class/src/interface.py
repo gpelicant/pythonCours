0c652f79415e809f3a6424517934a68da58e7cf4
gpelicant <gpelicant@tradelab.fr>
2019-04-03 14:31:50 +0200

    exo 4 part 3

---
+++ curriculum/01-Python-Foundations/03-Functions-Classes-and-Modules/04-Define-your-own-class/src/interface.py
@@ -0,0 +1,12 @@
+"""
+    my class video games
+"""
+
+class Game:
+    def __init__(self, support, genre, price):
+      self.support = support
+      self.type = genre
+      self.price = price
+
+final_fantasy = Game("sony", "RPG", 60)
+Apex = Car("microsoft", "FPS", 0)
