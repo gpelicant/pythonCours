a83e5d42879759483763fde6dbce08a12daf9331
gpelicant <gpelicant@tradelab.fr>
2019-04-03 14:21:38 +0200

    exo 3 part 3

--- curriculum/01-Python-Foundations/03-Functions-Classes-and-Modules/03-Taxi-Fare/src/taxi_fare.py
+++ curriculum/01-Python-Foundations/03-Functions-Classes-and-Modules/03-Taxi-Fare/src/taxi_fare.py
@@ -6,4 +6,25 @@ A module that implements a function to compute the fare of a taxi trip.
 
 
 # TODO: implement the compute_fare function.
-# NOTE: Don't forget to write a nice docsring
+# NOTE: Don't forget to write a nice docstring
+
+def compute_fare(distance, duration, drop_charge=2.60):
+    """
+    Returns the compute fare of a taxi.
+
+    Args
+    ----
+    distance: float
+    duration: float
+    drop_charge: float
+
+    Returns
+    -------
+    a float
+    """
+    distancePerMiles = 2.70
+    durationPerMinute = 0.50
+    calculDuration = duration * durationPerMinute
+    calculDistance = distance * distancePerMiles
+    total = drop_charge + calculDistance + calculDuration
+    return total
